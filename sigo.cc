211671d3bd5cb06259dc8a15f75936cd36554d32
Pedro Luis Espino <pedro@astro.physics.arizona.edu>
2016-07-13 16:35:06 -0700

    working version

--- sigo.cc
+++ sigo.cc
@@ -1,9 +1,9 @@
 #include "sigointegrators.hh"
 
 int main(){
-	double ScalarDensity, KineticEnergy, EnergyDensity, Int1, Int2, Int3;			
+	double KineticEnergy, EnergyDensity, Int1, Int2, Int3;			
 	double E0 = 0;	
-	double BaryonDensity = 0;				//the one true var
+	double BaryonDensity = 0*rho0;				//the one true var
 	double ConstC, ConstB, GSIGMA, GOMEGA, GRHO;
 	ofstream DAT;						//my file
 	DAT.open("SigOm.dat");
@@ -12,58 +12,71 @@ int main(){
 	double Etest = 0;
 	double kFermi = 0;
 	double kFermi2 = 0;
+	double SRtest = 0;
+	double ScalarDensity = 0;
+	double DelMass = 0;
+	double E01 = mass/MeVtoinvFM;
+	double E02 = mass/MeVtoinvFM;
+	double supress = 0;
+	cout << "supress rho: "; cin >> supress; cout << '\n';
 /*========== now use these to get the variable parameters ==================*/	
 	while (BaryonDensity <= 10*rho0){
-		kFermi = pow((6*pi*pi/4 *BaryonDensity), 0.3333333);
-		ScalarDensity = Int_rhoS(0, kFermi, 1000);
+		kFermi = pow((6*pi*pi/4 *BaryonDensity), (1./3));
 		KineticEnergy = Int_Ekinetic(0, kFermi, 1000);
 		Int1 = Int_eye1(0, kFermi, 1000);
 		Int2 = Int_eye2(0, kFermi, 1000);
 		Int3 = KineticEnergy;
 
-		ConstC = B(BaryonDensity, Int1, Int2, Int3, kFermi);
-		ConstB = C(BaryonDensity, Int1, Int2, Int3, kFermi);
+		ConstC = C(BaryonDensity, Int1, Int2, Int3, kFermi);
+		ConstB = B(BaryonDensity, Int1, Int2, Int3, kFermi);
 		GSIGMA = Gsigma2(BaryonDensity, ConstB, ConstC, Int1, kFermi);
 		GOMEGA = Gomega2(BaryonDensity, kFermi);
-		GRHO = Grho2(BaryonDensity, kFermi);
 		EnergyDensity = 0;
-		
-//		EnergyDensity += 0.5 * GSIGMA * pow(ScalarDensity,2);
-//		EnergyDensity += 0.5 * GOMEGA * pow(BaryonDensity,2);
-//		EnergyDensity += 0.5 * GRHO * pow(BaryonDensity,2);
-		EnergyDensity += KineticEnergy;
-//		EnergyDensity += 0.3333* ConstB*mass* pow(GSIGMA, 3)*pow(ScalarDensity, 3);
-//		EnergyDensity += 0.25* ConstC * pow(GSIGMA, 4)*pow(ScalarDensity,4);
+		Etest = 0;
+ 		SRtest = BaryonDensity *(1 - 3*kFermi*kFermi/(10*mass*mass));
+		ScalarDensity = Int_rhoS(0, kFermi, 1000);
+		DelMass = mass - mstar;
+		GRHO = Grho2(BaryonDensity, kFermi);
+	
+		EnergyDensity = 0.5*(GOMEGA)* BaryonDensity - 0.5*(GSIGMA)*pow(ScalarDensity, 2)/BaryonDensity + KineticEnergy/BaryonDensity - (1./3)*ConstB*mass*pow((mass-mstar), 3)/BaryonDensity - (1./4) * ConstC*pow(DelMass, 4)/BaryonDensity
+		+ 0.5*GRHO*BaryonDensity*supress; 
+		Etest = 0.5*(GOMEGA)* BaryonDensity - 0.5*(GSIGMA)*pow(SRtest, 2)/BaryonDensity + KineticEnergy/BaryonDensity - 0.3333*ConstB*mass*pow((mass-mstar), 3)/BaryonDensity - 0.25 * ConstC*pow(DelMass, 4)/BaryonDensity
+	+ 0.5*GRHO*BaryonDensity*supress;
 
-		Etest = 3*kFermi*kFermi/(10*mass);
-//		Etest +=0.5*(GOMEGA - GSIGMA)*BaryonDensity;
-//		Etest += GSIGMA*BaryonDensity/mass *(3*pow(kFermi, 3)/(10*mass));
-
-		if (BaryonDensity/rho0 < 1.01 && BaryonDensity/rho0 > 0.99){
-			cout << 
-			"r/ro: " << BaryonDensity/rho0<< '\n' << 
-			"E: " << (EnergyDensity/BaryonDensity/MeVtoinvFM)  << '\n' << 
-			"Etest: " << Etest/MeVtoinvFM << '\n' <<
-			"sig: " << GSIGMA << '\n' <<
-			"om: " << GOMEGA << '\n' <<
-			"rho: " << GRHO << '\n' << 
-			"b: " << B(BaryonDensity, Int1, Int2, Int3, kFermi) << '\n' << 
-			"c: " << C(BaryonDensity, Int1, Int2, Int3, kFermi) << '\n' << 
-			"b2: " << B2(BaryonDensity, Int1, Int2, Int3, kFermi) << '\n' << 
-			"c2: " << C2(BaryonDensity, Int1, Int2, Int3, kFermi) << '\n' << '\n';
+		if (BaryonDensity < 0.02 && BaryonDensity > 0.0){
+			E01 = EnergyDensity/MeVtoinvFM;
+			E02 = Etest/MeVtoinvFM;
 		}
-
+	
 		DAT << 
 		BaryonDensity/rho0<< " " << 
-		(EnergyDensity)  << " " << 
-		Etest << " " <<
-		GSIGMA << " " <<
+		(EnergyDensity)/MeVtoinvFM - E01 << " " << 
+		(Etest)/MeVtoinvFM - E02<< " " <<
+		-GSIGMA << " " <<
 		GOMEGA << " " <<
 		GRHO << " " <<
-		ConstB << " " <<
-		ConstC << " " <<
-		ScalarDensity << " " << 
-		kFermi << '\n'; 
+//		ConstB << " " <<
+//		ConstC << " " <<
+		GSIGMA*ScalarDensity*ScalarDensity<< " " << 
+		Int3 << " " << '\n';
+		if (BaryonDensity < 1.01*rho0 && BaryonDensity > 0.99*rho0){
+			cout << 
+			"r/ro: " << BaryonDensity/rho0<< '\n' << 
+			"E: " <<(EnergyDensity)/MeVtoinvFM - E01  << '\n' << 
+			"Etest: " << Etest/MeVtoinvFM - E02 << '\n' <<
+			"K: " << Kompress/MeVtoinvFM << '\n' << 
+			"sig: " << GSIGMA << '\n' <<
+			"om: " << GOMEGA << '\n' <<
+			"rho: " << GRHO << '\n' <<
+			"b: " << ConstB << '\n' << 
+			"c: " << ConstC << '\n' << 
+			"kFermi: " << kFermi << '\n' << '\n';
+			
+			double i1 = 0;
+			i1 = eye3(kFermi, mstar);
+				
+			cout << "int: " << Int3 << "    an: " << i1 << '\n' << '\n';
+		}
 		
 		BaryonDensity += drho;
 	}
--- sigofunctions.hh
+++ sigofunctions.hh
@@ -8,17 +8,17 @@
 #include <fstream>
 using namespace std;
 
-/*===============================================================================================================
-========= Emperical Nuclear parameters - working in units of MeV for energies and fm for distances ==============
-===============================================================================================================*/
-const double MeVtoinvFM = 0.00507;				// approximate conversion factor
+/*==============================================================================================================
+==== Emperical Nuclear parameters - working in units of MeV and fm^-1 for energies and fm for distances ========
+==============================================================================================================*/
+const double pi = M_PI;						// Pie (yummm)
+const double MeVtoinvFM = 0.0008065*(2*pi);			// approximate conversion factor
 const double rho0 = 0.153;					// staturation density (fm^-3)
-const double Kompress = 200*MeVtoinvFM;				// Compression modulus (fm^-1)
+double Kompress = 250*MeVtoinvFM;				// Compression modulus (fm^-1)
 double mass = 938*MeVtoinvFM;					// nucleon mass (fm^-1) 
-double mstar = 0.7*mass;					// effective mass
+double mstar = 0.75*mass;					// effective mass
 const double asymm = 32.5*MeVtoinvFM;				// symmetry energy coefficient (fm^-1)
 const double BperA = -16.3*MeVtoinvFM;				// Binding energy (fm^-1)
-const double pi = M_PI;						// Pie (yummm)
 
 /*===============================================================================================================
 ================================== Constant parameters at saturation ============================================
@@ -27,13 +27,17 @@ double Gomega2(double RHO, double kF){
 	double fieldstrength = (mass + BperA - sqrt(kF*kF + mstar * mstar))/RHO;	
 	return fieldstrength;
 }
+double Grho2(double RHO, double kF){
+	double fieldstrength = 8*(asymm - kF*kF/(6*sqrt(kF*kF + mstar*mstar)))/RHO;
+	return fieldstrength;
+}
 
 double alpha1(double RHO, double kF){
 	double a1 = Kompress - Gomega2(RHO, kF)*6*pow(kF, 3)/(pi*pi) - 3 * kF*kF/sqrt(kF*kF + mstar*mstar);
 	return a1;
 }
 double beta1(double RHO, double kF){
-	double b1 = 2*mass*(mass - mstar)*alpha1(RHO,kF);	//NOTE: I CHANGED WHAT beta1 IS FROM LIT.
+	double b1 = 2*mass*(mass - mstar)*alpha1(RHO,kF);	
 	return b1;
 }
 double gamma1(double RHO, double kF){
@@ -46,20 +50,20 @@ double del1(double RHO, double IONE, double kF){
 }
 
 
-double alpha2 = (mass - mstar);
-double beta2 = mass * pow((mass - mstar),2);
-double gamma2 = pow((mass - mstar),3);
-double del2(double ITWO){
-	double d2 = ITWO;
-	return ITWO;
+double alpha3 = (mass - mstar);
+double beta3 = mass * pow((mass - mstar),2);
+double gamma3 = pow((mass - mstar),3);
+double del3(double ITHREE){
+	double d3 = ITHREE;
+	return ITHREE;
 }
 
-double alpha3 = 0.5 * pow((mass - mstar),2);
-double beta3 = (1/3) * mass * pow((mass - mstar),3);
-double gamma3 = 0.25 * pow((mass - mstar),4);
-double del3(double RHO, double ITHREE, double kF){
-	double d3 = RHO* (mass + BperA) - ITHREE - 0.5 * Gomega2(RHO, kF) * pow(RHO,2);
-	return d3;
+double alpha2 = 0.5 * pow((mass - mstar),2);
+double beta2 = (1/3) * mass * pow((mass - mstar),3);
+double gamma2 = 0.25 * pow((mass - mstar),4);
+double del2(double RHO, double ITWO, double kF){
+	double d2 = RHO* (mass + BperA) - ITWO - 0.5 * Gomega2(RHO, kF) * pow(RHO,2);
+	return d2;
 }
 
 
@@ -72,8 +76,8 @@ double C(double RHO, double IONE, double ITWO, double ITHREE, double kF){
 	double ag21 = ( alpha2*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma2 );
 	double ag31 = ( alpha3*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma3 );
 	
-	double ad31 = ( alpha3*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del3(RHO, ITHREE, kF) );
-	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(ITWO) );
+	double ad31 = ( alpha3*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del3(ITHREE) );
+	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(RHO, ITWO, kF) );
 
 	double ab31 = ( alpha3*beta1(RHO, kF) - alpha1(RHO, kF)*beta3 );
 	double ab21 = ( alpha2*beta1(RHO, kF) - alpha1(RHO, kF)*beta2 );
@@ -89,8 +93,8 @@ double B(double RHO, double IONE, double ITWO, double ITHREE, double kF){
 	double ag21 = ( alpha2*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma2 );
 	double ag31 = ( alpha3*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma3 );
 	
-	double ad31 = ( alpha3*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del3(RHO, ITHREE, kF) );
-	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(ITWO) );
+	double ad31 = ( alpha3*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del3(ITHREE) );
+	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(RHO, ITWO, kF) );
 
 	double ab31 = ( alpha3*beta1(RHO, kF) - alpha1(RHO, kF)*beta3 );
 	double ab21 = ( alpha2*beta1(RHO, kF) - alpha1(RHO, kF)*beta2 );
@@ -102,45 +106,6 @@ double B(double RHO, double IONE, double ITWO, double ITHREE, double kF){
 	return b;
 }
 
-double B2(double RHO, double IONE, double ITWO, double ITHREE, double kF){
-	double ag21 = ( alpha2*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma2 );
-	double ag32 = ( alpha3*gamma2 - alpha2*gamma3 );
-	
-	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(ITWO) );
-	double ad32 = ( alpha3*del2(ITWO) - alpha2*del3(RHO, ITHREE, kF) );
-
-	double ab21 = ( alpha2*beta1(RHO, kF) - alpha1(RHO, kF)*beta2 );
-	double ab32 = ( alpha3*beta2 - alpha2*beta3 );
-
-	double bnumerator = ag21*ad32 - ag32*ad21;
-	double bdenominator = ag21*ab32 - ag32*ab21;
-
-	double b = bnumerator/bdenominator;
-	return b;
-}
-
-double C2(double RHO, double IONE, double ITWO, double ITHREE, double kF){
-	double ag21 = ( alpha2*gamma1(RHO, kF) - alpha1(RHO, kF)*gamma2 );
-	double ag32 = ( alpha3*gamma2 - alpha2*gamma3 );
-	
-	double ad21 = ( alpha2*del1(RHO, IONE, kF) - alpha1(RHO, kF)*del2(ITWO) );
-	double ad32 = ( alpha3*del2(ITWO) - alpha2*del3(RHO, ITHREE, kF) );
-
-	double ab21 = ( alpha2*beta1(RHO, kF) - alpha1(RHO, kF)*beta2 );
-	double ab32 = ( alpha3*beta2 - alpha2*beta3 );
-
-	double cnumerator = ad32 - ab32*B2(RHO, IONE, ITWO, ITHREE, kF);
-	double cdenominator = ag32;
-
-	double c = cnumerator/cdenominator;
-	return c;
-}
-/*===================================================================================================*/
-double Grho2(double RHO, double kF){
-	double fieldstrength = asymm - kF*kF/(6*sqrt(kF*kF + mstar*mstar));
-	fieldstrength *= 8/RHO;
-	return fieldstrength;
-}
 
 double Gsigma2(double RHO, double CONSTB, double CONSTC, double IONE, double kF){
 	double sigmanumerator = alpha1(RHO, kF);
@@ -196,4 +161,21 @@ double goko(double k, void *param){			//Kinetic Energy Integrand-same form as I3
 	Integrand = (2/(pi*pi)) * k2* E;
 	return Integrand;
 }
+double eye1(double kF, double m){
+	double x = kF/m;
+	double t = sqrt(1 + x*x);
+	return 2/(pi*pi) * m*m*(0.5*x*t + x/t - (1.5)*log(x+t));
+}
+	 
+double eye2(double kF, double m){
+	double x = kF/m;
+	double t = sqrt(1 + x*x);
+	return 1./(pi*pi) * pow(m, 3)*(x*t - log(x+t));
+}
+double eye3(double kF, double m){
+	double x = kF/m;
+	double t = sqrt(1 + x*x);
+	return 1./(2*pi*pi) * pow(m, 4)*(-0.5*x*t + x*pow(t, 3) - 0.5*log(x+t));
+}
+	
 
